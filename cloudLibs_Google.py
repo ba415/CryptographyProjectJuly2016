d85ff535e96b9873ac170cfff54c2b7689c02e60
timewrinkle <timewrinkle@users.noreply.github.com>
2016-07-25 20:57:32 -0400

    added key file getters and setters and minor clean ups

--- cloudLibs_Google.py
+++ cloudLibs_Google.py
@@ -5,7 +5,6 @@ Created on Jul 16, 2016
 '''
 import sys
 from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 #For walking the file directory structure
 from pydrive_ex.drive import GoogleDrive 
 
@@ -16,36 +15,53 @@ class gb_cloud():
         gauth = GoogleAuth()
         gauth.LocalWebserverAuth() #creates local web server
         self.drive = GoogleDrive(gauth)       
-    
-    def disconnect(self):
-        print 'disconnect to google drive'
         
-    def upload(self,localFile, cloudFile):
+    def upload(self,localFilePath, cloudFilePath):
+        #upload from the local user space to the google cloud
         try:
             print 'Upload Starting'
-            self.drive.uploadFile(cloudFile, localFile)
+            self.drive.uploadFile(cloudFilePath, localFilePath)
             print 'Upload Complete ' 
         except:
-            print 'Upload Failed'
-            print 'Unexpected Error:', sys.exc_info()[0]
+            print 'Upload Failed, Unexpected Error:', sys.exc_info()[0]
             raise   
             
-    #Get file from google drive
-    def download(self,cloudFile,localFile):
+    def download(self,cloudFilePath,localFilePath):
+        #download from the cloud to your local user space
         try:
             print 'Download Starting'
-            self.drive.downloadFile(cloudFile, localFile)
+            self.drive.downloadFile(cloudFilePath, localFilePath)
             print 'Download Complete ' 
         except:
-            print 'Download Failed'
-            print 'Unexpected Error:', sys.exc_info()[0]
+            print 'Download Failed, Unexpected Error:', sys.exc_info()[0]
             raise
+        
+    def putPublicKey(self,localKeyFile, cloudKeyFile):
+        #upload from the local user space to the google cloud
+        try:
+            print 'Upload of public keys Starting'
+            self.drive.uploadFile(cloudKeyFile, localKeyFile)
+            print 'Upload Complete ' 
+        except:
+            print 'Upload of public keys Failed, Unexpected Error:', sys.exc_info()[0]
+            raise   
+            
+    def getPublicKey(self,cloudKeyFile,localKeyFile):
+        #download from the cloud to your local user space
+        try:
+            print 'Download of public keys Starting'
+            self.drive.downloadFile(cloudKeyFile, localKeyFile)
+            print 'Download Complete ' 
+        except:
+            print 'Download public keys Failed, Unexpected Error:', sys.exc_info()[0]
+            raise    
+        
 
 def main(argv):    
     #create object handle to test functions
     handle = gb_cloud()
-    handle.upload('upload/moonTest.jpg','test2/images/moon123.jpg')
-    handle.download('test2/images/moon.jpg','download/moonDL.jpg')   
+    handle.upload('upload/CryptographyCloudProject-draft.pptx','CryptoProjectDrive/CryptographyCloudProject-draft.pptx')
+    #handle.download('CryptoProjectDrive/moon1.jpg','download/moon1.jpg')   
    
 
 if __name__ == '__main__':
